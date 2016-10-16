ed43e21632d60180b03d6ad93e7164b74cdfd56c
mr.debbie <giyoungchong1980@gmail.com>
2016-10-15 19:40:05 -0500

    update routes with new controllers

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,7 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+var ctrlCloud = require('../controllers/clouds.js');
+var ctrlRunway = require('../controllers/runway.js');
+var ctrlEdits = require('../controllers/stores.js');
 /* GET home page. */
-router.get('/', ctrlMain.index);
+router.get('/', ctrlCloud.home);
+router.get('/art', ctrlCloud.art);
+router.get('/health', ctrlCloud.health);
+router.get('/readme', ctrlCloud.readmes);
+router.get('/runway', ctrlRunway.runway);
+router.get('/store', ctrlEdits.storedit);
+router.get('/bonus', ctrlEdits.bonusedit);
+
+
+
+
+
 
 module.exports = router;
