5a51fb896a1e84f5cfc57b99878cbe7a988c477d
mr.debbie <giyoungchong1980@gmail.com>
2016-12-02 11:17:44 -0600

    update route and controller for user profile

--- app_server/controllers/profile.js
+++ app_server/controllers/profile.js
@@ -1,3 +1,7 @@
 /**
  * Created by debrachong on 12/2/16.
  */
+/* GET home page */
+module.exports.createUser = function(req,res){
+    res.render('profile', { title: 'new user page'});
+};
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var ctrlCloud = require('../controllers/clouds.js');
 var ctrlRunway = require('../controllers/runway.js');
 var ctrlEdits = require('../controllers/stores.js');
+var ctrlProfile = require('../controllers/profile.js');
+
 /* GET home page. */
 router.get('/', ctrlCloud.home);
 router.get('/art', ctrlCloud.art);
@@ -11,6 +13,8 @@ router.get('/readme', ctrlCloud.readmes);
 router.get('/runway', ctrlRunway.runway);
 router.get('/store', ctrlEdits.storedit);
 router.get('/bonus', ctrlEdits.bonusedit);
+router.get('/createProfile', ctrlProfile.createUser);
+
 
 
 
