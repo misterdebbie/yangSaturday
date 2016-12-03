4f2881803494586258e097d7dfe3e06f595e6a3a
mr.debbie <giyoungchong1980@gmail.com>
2016-12-03 08:51:08 -0600

    add request module and api default/URL base

--- app_server/controllers/profile.js
+++ app_server/controllers/profile.js
@@ -1,6 +1,34 @@
 /**
  * Created by debrachong on 12/2/16.
  */
+var request = require('request');
+var apiOptions = {
+    server : "http://localhost:3000"
+};
+if (process.env.NODE_ENV === 'production') {
+    apiOptions.server = "https://getting-mean-loc8r.herokuapp.com";
+}
+
+var _showError = function (req, res, status) {
+    var title, content;
+    if (status === 404) {
+        title = "404, page not found";
+        content = "Oh dear. Looks like we can't find this page. Sorry.";
+    } else if (status === 500) {
+        title = "500, internal server error";
+        content = "How embarrassing. There's a problem with our server.";
+    } else {
+        title = status + ", something's gone wrong";
+        content = "Something, somewhere, has gone just a little bit wrong.";
+    }
+    res.status(status);
+    res.render('generic-text', {
+        title : title,
+        content : content
+    });
+};
+
+
 /* GET home page */
 module.exports.createUser = function(req,res){
     res.render('profile', { title: 'new user page'});
