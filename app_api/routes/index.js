a24e4e6957b90acbc41397c41157fb7a83d7a0c4
mr.debbie <giyoungchong1980@gmail.com>
2017-06-21 21:01:10 -0500

    yang test stop

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,17 +1,17 @@
 /**
  * Created by debrachong on 12/2/16.
  */
-var express = require('express');
+/*var express = require('express');
 var router = express.Router();
-var ctrlUsers = require('../controllers/users');
+var ctrlUsers = require('../controllers/users');*/
 //var ctrlWish = require('../controllers/wish');
 
 
-router.get('/users', ctrlUsers.displayUsers);
+/*router.get('/users', ctrlUsers.displayUsers);
 router.post('/users', ctrlUsers.userCreate);
 router.get('/users/:userId', ctrlUsers.userReadOne);
 router.put('/users/:userId', ctrlUsers.userUpdateOne);
-router.delete('/users/:userId', ctrlUsers.userDeleteOne);
+router.delete('/users/:userId', ctrlUsers.userDeleteOne);*/
 
 /*// reviews
 router.post('/items/:itemId/bids', ctrlBids.bidsCreate);
@@ -19,4 +19,4 @@ router.get('/items/:itemId/bids/:bidId', ctrlBids.bidsReadOne);
 router.put('/items/:itemId/bids/:bidId', ctrlBids.bidsUpdateOne);
 router.delete('/items/:itemId/bids/:bidId', ctrlBids.bidsDeleteOne);*/
 
-module.exports = router;
+//module.exports = router;
